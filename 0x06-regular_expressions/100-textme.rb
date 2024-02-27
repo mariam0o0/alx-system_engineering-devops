1f6b136c56ca7c6bff10c60fe7ef12eb88224814
Mariam <mariam.yasser772000@gmail.com>
2024-02-27 14:06:35 -0800

    0x06

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 #The regular expression that matches a pattern
 
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*)\]\s\[to:(.*)\]\s\[flags:(.*?)\]/).join(',')
