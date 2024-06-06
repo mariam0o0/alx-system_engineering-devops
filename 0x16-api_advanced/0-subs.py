fd6cf06cd0d86fe788cd5f0c18d19074fc3c7a27
Mariam <mariam.yasser772000@gmail.com>
2024-06-06 07:25:59 -0700

    0x16

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for number_of_subscribers"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers"""
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+    return sub_info.json().get("data").get("subscribers")
